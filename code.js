672470dc9809076c675c4af7ca5a01a7d2c843dc
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-01-30 14:37:48 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function insertionSortReverse(array) {
     for (var i = (array.length - 2); i >= 0; i--) {
         var val = array[i];
         var j; 
-        for (j = i; j <= array.length && array[j + 1] >= val; j++) {
+        for (j = i; j >= 0 && array[j + 1] >= val; j++) {
             array[j] = array[j + 1]; 
         }
         array[j] = val; 
